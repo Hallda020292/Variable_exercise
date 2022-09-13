031f2ba437d8fc3dc16c344e76c7b409e5e343cb
Dakota hall <Hallda020292@gmail.com>
2022-09-13 14:19:03 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variable_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string dogName = "Ralph";
+            int dogAge = 10;
+            Console.WriteLine($"my dog's name is {dogName}, He is {dogAge} years old");
+
+            
+        }
+    }
+}
